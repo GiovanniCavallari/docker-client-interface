602dc568da6ea6b3d6082574cf5b69e03b215091
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2022-08-03 10:35:31 -0300

    feat: added react-router-dom

--- frontend/src/components/templates/Admin/index.jsx
+++ frontend/src/components/templates/Admin/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Props from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import { Container, Footer, Header } from 'rsuite';
+import { routes } from '../../../enums/routes';
 
 import Logo from '../../atoms/Logo';
 import MadeBy from '../../atoms/MadeBy';
@@ -9,13 +11,15 @@ import SidebarMenu from '../../molecules/SideBar';
 import './styles.less';
 
 const AdminTemplate = ({ children }) => {
+  const navigate = useNavigate();
+
   return (
     <Container className="di-admin-template-container">
       <Header className="di-header">
         <Logo
           alt="docker logo"
           src="https://www.docker.com/wp-content/uploads/2022/03/Docker-Logo-White-RGB_Horizontal.png"
-          onClick={() => {}}
+          onClick={() => navigate(routes.INDEX)}
         />
       </Header>
 
