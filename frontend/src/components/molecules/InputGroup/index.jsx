6c6aff0951f0e6845ab238e204f17f947b4e31c5
Giovanni Cavallari <giovanni.cavallari@mercadolivre.com>
2022-08-05 12:35:42 -0300

    feat: improvements

--- frontend/src/components/molecules/InputGroup/index.jsx
+++ frontend/src/components/molecules/InputGroup/index.jsx
@@ -6,11 +6,13 @@ import Input from '../../atoms/Input';
 
 const InputGroup = ({ first, second, onChange, className, separator, hasSeparator = false }) => {
   const spacing = first.label || second.label ? 'di-top-32' : 'di-top-16';
+  const firstStyle = first.uppercase ? { textTransform: 'uppercase' } : {};
+  const secondStyle = second.uppercase ? { textTransform: 'uppercase' } : {};
 
   return (
     <FlexboxGrid className={className}>
       <FlexboxGrid.Item colspan={11} className="di-colspan-12">
-        <Input name={first.name} value={first.value} label={first.label} onChange={onChange} />
+        <Input name={first.name} value={first.value} label={first.label} onChange={onChange} style={firstStyle} />
       </FlexboxGrid.Item>
 
       {hasSeparator && (
@@ -20,7 +22,7 @@ const InputGroup = ({ first, second, onChange, className, separator, hasSeparato
       )}
 
       <FlexboxGrid.Item colspan={11} className="di-colspan-12">
-        <Input name={second.name} value={second.value} label={second.label} onChange={onChange} />
+        <Input name={second.name} value={second.value} label={second.label} onChange={onChange} style={secondStyle} />
       </FlexboxGrid.Item>
     </FlexboxGrid>
   );
@@ -30,6 +32,7 @@ const inputProps = Props.shape({
   name: Props.string.isRequired,
   value: Props.oneOfType([Props.string, Props.number]).isRequired,
   label: Props.string,
+  uppercase: Props.bool,
 });
 
 InputGroup.propTypes = {
